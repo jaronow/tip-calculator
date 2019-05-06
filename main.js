ba0048e5d8b3b6b7c3c6551a0749de2b01549558
Will Aronow <john.aronow@gmail.com>
2019-05-05 17:22:46 -0700

    added check for values in total amount and service

--- main.js
+++ main.js
@@ -2,4 +2,8 @@ function calcTip() {
   let billAmt = document.getElementById("total").value;
   let service = document.getElementById("service").value;
   let peeps = document.getElementById("people").value;
-}
+
+  if (billAmt === "" || service === 0) {
+    alert("Please input bill amount and/or service quality");
+  };
+};
