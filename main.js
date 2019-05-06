28e1dd81c4c5e9893543c7b4944a8612f31b8d3a
Will Aronow <john.aronow@gmail.com>
2019-05-05 17:14:05 -0700

    fixed total amount input to accept decimals and added start of JS code to accept input values from form

---
+++ main.js
@@ -0,0 +1,5 @@
+function calcTip() {
+  let billAmt = document.getElementById("total").value;
+  let service = document.getElementById("service").value;
+  let peeps = document.getElementById("people").value;
+}
